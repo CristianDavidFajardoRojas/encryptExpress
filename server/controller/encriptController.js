120cf3d414f66bbf718c5c8558f821d23dc4b353
CristianDavidFajardoRojas <cfajardorojas98@gmail.com>
2024-10-02 09:03:55 -0500

    feat: :sparkles: UI Mostrat contraseña encriptada

--- server/controller/encriptController.js
+++ server/controller/encriptController.js
@@ -5,17 +5,8 @@ exports.encriptText = async (req, res) => {
         const encriptModelClass = new encriptModel()
 
         let resEncript = await encriptModelClass.encriptText(req.body.message);
-        // const movie = new Movie(req.data);
-        
-        // let resMovie = await movie.getMovieById(req.params.id);
-        // const salas = new Salas(req.data);
-        // let resSalas = await salas.getSalaById(req.params.idSala);
-        // const funciones = new Funcion(req.data);
-        // let resFunciones = await funciones.getFuncionesBySalaAndMovie(req.params.id, req.params.idSala)
-
-
-        // if(resMovie.status == 200) return res.status(resMovie.status).json(resMovie);
-        // return res.status(resMovie.status).json(resMovie);
+     
+        return res.status(resEncript.status).json(resEncript);
     } catch (error) {
         let err = JSON.parse(error.message);
         if(err.status == 500) return res.status(err.status).json(err);
--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 let input_text = document.querySelector(".input_text");
 let encriptar_button = document.querySelector("#encriptar_button");
+let section_derecha = document.querySelector("#section_derecha");
 
 let infoIngresada
 
@@ -7,12 +8,28 @@ let infoIngresada
         infoIngresada = (e.target.value); 
     })
 
+
+
+
 encriptar_button.addEventListener('click', async()=> {
+    if(infoIngresada != ''){
     let peticion = await fetch(`${location.origin}/encript/v1`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({message: infoIngresada}) });
 
     let res = await peticion.json();
-    console.log(res)
+    
+    section_derecha.className = "Yes_text"
+    section_derecha.innerHTML = /*html*/`<p>${res.data}</p>`;
+}else {
+    section_derecha.className = "No_text"
+    section_derecha.innerHTML = /*html*/`
+        <img src="../storage/img/Muñeco.png">
+        <h3>Ningún mensaje fue encontrado</h3>
+        <p>Ingresa el texto que desees encriptar o desencriptar</p>
+    `;
+}
 })
+
+ 
